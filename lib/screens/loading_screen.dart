85533ec2008148b406e782589964d464fb32cbe1
SamatarMumin <35471795+SamatarMumin@users.noreply.github.com>
2023-01-31 17:02:02 -0500

    Moving and reformating Loading screen
    Adding loading spinkit, moving network related functions in its own networkHelper class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:cimilada/services/location.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:cimilada/services/network.dart';
+import 'location_screen.dart';
+
+const apikey = '1e43bc8ef425f7bd1a9d0422b85cb35a';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
- void getLocation() async{
-  Location location = new Location();
-  return await location.getCurrentLocation();
-}
-
-void getData() async{
-  Response response = await http.get(Uri.parse('http://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}'));
-  print(response);
-}
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = new Location();
+    await location.getCurrentLocation();
+    lat = location.latitude;
+    long = location.longitude;
+
+    Network networkResponse = new Network(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apikey&units=metric');
+
+    var weatherData = await networkResponse.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen(locationWeather: weatherData);
+    }));
+
+   
+  }
+
+  @override
   void initState() {
     getLocation();
   }
 
+  late double lat;
+  late double long;
+
   @override
   Widget build(BuildContext context) {
-    getData();
-    return Scaffold();
+    return Scaffold(
+     body: SpinKitSquareCircle(
+       color: Colors.white,
+       size: 50.0,
+       ),
+
+    );
   }
 }
+
+/*
+
+     print(city);
+    print(weatherdesc);
+ */
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,17 +1,38 @@
+import 'package:cimilada/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cimilada/util/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  late final locationWeather;
+  LocationScreen({this.locationWeather});
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
+
 class _LocationScreenState extends State<LocationScreen> {
+  var temp;
+  var condition;
+  var city;
+
+  void initState(){
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(var weatherData){
+    condition = weatherData['weather'][0]['id'];
+    temp = weatherData['main']['temp'].toInt();
+    city = weatherData['name'];
+    print(temp);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      body: Center(
+        child: Container(
+            child: Text('$temp'),
+        ),
       ),
     );
   }
